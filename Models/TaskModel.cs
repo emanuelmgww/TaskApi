55c85d5701115430cb7f95447005eb7b2d2f8696
emanuelmgww <emanuelmirandagaldino40197@gmail.com>
2025-03-12 14:01:01 -0300

    Criação do modelo de dados de uma Tasl

---
+++ Models/TaskModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskApi.Models
+{
+    public class TaskModel
+    {
+        public Guid Id { get; private set; }
+        public string Descricao { get; set; }
+
+        public TaskModel(string descricao)
+        {
+            Descricao = descricao;
+        }
+    }
+}
